d7e95fa33ed6d67f34781df6e686b31ccdf2b38d
StalkerBrig <32149616+StalkerBrig@users.noreply.github.com>
2017-09-25 11:08:54 -0400

    Update cbc-enc.java

--- cbc-enc.java
+++ cbc-enc.java
@@ -1,43 +1,16 @@
-import java.util.LinkedList;
-
-
-
 public class Main
 {
 	public static void main(String[] args) throws Exception
 	{
-		String test = "1234567890abcdef";
-		String key_file;
-		String input_file;
-		String iv_file;
+		byte[] key_data;
+		byte[] input_data;
+		byte[] iv_data;
 		
 		
-		key_file = ctfuncs.key_file(args);
-		input_file = ctfuncs.input_file(args);
-		iv_file = ctfuncs.iv_file(args);
+		key_data = ctfuncs.key_file(args);
+		input_data = ctfuncs.input_file(args);
+		iv_data = ctfuncs.iv_file(args);
 		
-		System.out.println(key_file);
-		System.out.println(input_file);
-
-		
-		//checks to see if an iv file existed
-		// if it doesn't, we generate a new iv
-		if ( iv_file.equals( "-1" ) )
-		{
-			//Make IV
-			LinkedList<Integer> IVrand = new LinkedList<Integer>();
-			ctfuncs.IVgen(IVrand);
-			
-			//test IV
-			for(int i : IVrand)
-			{
-				//System.out.print(i);
-			}
-			
-			//System.out.println();
-		}
-		
-		System.out.println(iv_file);
 		
 		//encrypt
 		//getBytes returns an array of bytes
