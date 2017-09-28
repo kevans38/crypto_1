6c6a8d9f1284ceee08fcd8f225f963f0e23bc855
kevans38 <31311258+kevans38@users.noreply.github.com>
2017-09-28 08:31:56 -0400

    Added comments

--- cbcenc.java
+++ cbcenc.java
@@ -20,12 +20,14 @@ public class cbcenc
 		
 		int input_size;
 		
+		//open files from cmd line args
 		key_data = ctfuncs.key_file(args);
 		input_data = ctfuncs.input_file(args);
 		input_size = input_data.length;
 		
 		iv_data = ctfuncs.iv_file(args);
-						
+		
+		//create blocks and pad - using 128 bit blocks
 		input_blocks = ctfuncs.make_blocks(input_data, input_size);
 		
 		input_blocks = ctfuncs.padding(input_blocks, input_size);
@@ -35,6 +37,7 @@ public class cbcenc
 		
 		cipher_text = ctfuncs.append_bytes(cipher_text, iv_data, 0);
 		
+		//XOR Message: 1st with IV, then remaining with ciphertext
 		for ( int i = 0; i < input_blocks.length; i++ )
 		{
 			if ( i == 0 )
@@ -52,6 +55,7 @@ public class cbcenc
 
 		}
 		
+		//create encrypted file
 		ctfuncs.output_file(args, cipher_text );
 	}
 }
